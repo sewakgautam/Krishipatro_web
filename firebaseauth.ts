150daace0e1f4276e4f504e621d7c864c28cb114
Sewak Gautam <Sewak.gautam58@gmail.com>
2025-07-26 12:47:59 +0100

    transferred all into env file

--- firebaseauth.ts
+++ firebaseauth.ts
@@ -9,14 +9,14 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_fN7h4qzwciWrX36TcgyW5vKv8qTmRjk",
-  authDomain: "krishipatronp.firebaseapp.com",
-  databaseURL: "https://krishipatronp-default-rtdb.firebaseio.com",
-  projectId: "krishipatronp",
-  storageBucket: "krishipatronp.firebasestorage.app",
-  messagingSenderId: "875477129836",
-  appId: "1:875477129836:web:bbae5f8fd4039c7306d649",
-  measurementId: "G-MJR08JG976"
+  apiKey: process.env.NEXT_PUBLIC_FIRE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIRE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIRE_DATABASE,
+  projectId: process.env.NEXT_PUBLIC_FIRE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIRE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIRE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIRE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIRE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
